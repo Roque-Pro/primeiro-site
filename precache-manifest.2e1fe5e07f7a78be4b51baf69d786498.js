69e1af59c9c96c3770f98c599beaefb617f87fb7
Roque-Pro <65265702+Roque-Pro@users.noreply.github.com>
2020-06-04 23:41:20 -0300

    Add files via upload

---
+++ precache-manifest.2e1fe5e07f7a78be4b51baf69d786498.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6034c8e2c7446512bead4e7d471fba6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053b2c05ba7894162aa0",
+    "url": "/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "a47836cc4f7be44a1e45",
+    "url": "/static/css/main.27fc0e34.chunk.css"
+  },
+  {
+    "revision": "053b2c05ba7894162aa0",
+    "url": "/static/js/2.2419d598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2419d598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47836cc4f7be44a1e45",
+    "url": "/static/js/main.32bc5b0f.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
